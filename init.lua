ff3e91c9eaf2aaf4d00320288d7079b733dd4591
andy <andyach88@gmail.com>
2024-05-12 20:14:04 -0500

    Add ts/js lsp and html lsp

--- init.lua
+++ init.lua
@@ -486,7 +486,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
+        html = {},
         --
 
         lua_ls = {
